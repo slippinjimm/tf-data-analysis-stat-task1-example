9e797ac64c67935d361e0e37fe44423662918805
slippinjimm <118850232+slippinjimm@users.noreply.github.com>
2023-04-08 00:12:06 +0400

    Create solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,12 @@ import numpy as np
 chat_id = 123456 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
+    def solution(x: np.array) -> float:
+    t=10
+    n = len(x)
+    mu = -43
+    sigma = np.exp(1) 
+    error_v = np.random.normal(mu, sigma, n) # ошибки
+    a = error_v/t 
+    print(a)
     return x.mean() # Ваш ответ
